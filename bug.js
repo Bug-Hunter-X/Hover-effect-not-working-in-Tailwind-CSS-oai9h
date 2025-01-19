0b538ec22c050da3259b74117e7185b323ef5ac1
BugHunter-X <askzenai@gmail.com>
2025-01-19 16:24:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+//This is a sample code for Tailwind CSS causing the error.
+/*This is a sample comment*/
+<div class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Hover over me!
+</div>
+```
